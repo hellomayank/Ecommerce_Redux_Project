ee2053b2616d563035ce621c72e887162694a1b2
MAYANK816 <mayank0218.cse19@chitkara.edu.in>
2022-04-04 16:46:48 +0530

    Ecommerce_Redux_project

--- src/containers/Header.js
+++ src/containers/Header.js
@@ -5,13 +5,13 @@ const Header = () => {
         <>
             <Navbar bg="light" expand="lg">
                 <Container>
-                    <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+                    <Navbar.Brand href="/">Nikolas</Navbar.Brand>
                     <Navbar.Toggle aria-controls="basic-navbar-nav" />
                     <Navbar.Collapse id="basic-navbar-nav">
                         <Nav className="me-auto">
-                            <Nav.Link href="#home">Home</Nav.Link>
-                            <Nav.Link href="#link">Link</Nav.Link>
-
+                            <Nav.Link href="/">Home</Nav.Link>
+                            <Nav.Link href="/about">About Us</Nav.Link>
+                            <Nav.Link href="/contactUs">Contact Us</Nav.Link>
                         </Nav>
                     </Navbar.Collapse>
                 </Container>
--- src/containers/ProductDetail.js
+++ src/containers/ProductDetail.js
@@ -7,16 +7,31 @@ const ProductDetail = () => {
     const products = useSelector((state) => state.allProducts.products);
     const newProduct = products[product.productId];
     console.log(newProduct);
-    // console.log(product.productId);
+
     return (
-        <div className='mainDetail'>
-            <div className='mainDetail_product'>
-                <img src={newProduct.image}></img>
-                <h3></h3>
-                <h3></h3>
-                <h3></h3>
+        <>
+            <div className='mainDetail'>
+                <div className='mainDetail_product'>
+                    <img src={newProduct.image} style={{ width: "300px" }}></img>
+                    <div className='mainDetail'>
+                        <div className='mainDetail_product2'>
+                            <h5>{newProduct.title}</h5>
+                        </div>
+                        <div className='mainDetail_product2'>
+                            <button id="btn_atc">Add to cart</button>
+                            <button id="btn_bn">Buy Now</button>
+                        </div>
+                        <h5>Category : {newProduct.category}</h5>
+                        <h5>Price : ${newProduct.price}</h5>
+                    </div>
+
+
+
+                </div>
+
             </div>
-        </div>
+
+        </>
     )
 }
 
